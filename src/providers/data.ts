9a6afd0afeca40ede4a5d4f2c484e82010b79dd7
aldoSN003 <j.aldosn05@gmail.com>
2026-04-14 17:24:27 -0600

    implement data provider

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,5 +1,20 @@
 import { createSimpleRestDataProvider } from "@refinedev/rest/simple-rest";
-import { API_URL } from "./constants";
-export const { dataProvider, kyInstance } = createSimpleRestDataProvider({
-  apiURL: API_URL,
+import {API_URL, MOCK_SUBJECTS} from "./constants";
+import {BaseRecord, GetListParams, GetListResponse} from "@refinedev/core";
+export const  dataProvider = ({
+getList:async<TData extends BaseRecord = BaseRecord>({resource}:GetListParams):Promise<GetListResponse<TData>> =>{
+    if(resource!=="subjects"){
+        return {data:[] as TData[], total:0}
+    }
+
+    return {data:MOCK_SUBJECTS as unknown as TData[], total:MOCK_SUBJECTS.length};
+
+
+},
+    getOne: async()=> {throw new Error("Function not implemented.")},
+    create: async()=> {throw new Error("Function not implemented.")},
+    update: async()=> {throw new Error("Function not implemented.")},
+    deleteOne: async()=> {throw new Error("Function not implemented.")},
+
+    getApiUrl:()=>API_URL,
 });
